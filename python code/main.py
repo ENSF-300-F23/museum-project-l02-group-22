39e75bd965f798950c247afa9bc23963d06bf084
MOYOSOREJOBI <139525574+MOYOSOREJOBI@users.noreply.github.com>
2023-12-05 12:36:28 -0700

    Update main.py

--- python code/main.py
+++ python code/main.py
@@ -6,12 +6,19 @@ from art_object_manager import ArtObjectManager
 from interface import Interface
 
 def main():
-    db_manager = DatabaseManager("your_host", "your_user", "your_password", "your_database")
-    user_manager = UserManager(db_manager)
-    art_object_manager = ArtObjectManager(db_manager)
-    interface = Interface(user_manager, art_object_manager)
+    try:
+        db_manager = DatabaseManager("your_host", "your_user", "your_password", "your_database")
+        user_manager = UserManager(db_manager)
+        art_object_manager = ArtObjectManager(db_manager)
+        interface = Interface(user_manager, art_object_manager)
 
-    # Add main application logic here
+        while True:
+            interface.display_menu()
+            user_choice = input("Enter your choice: ")
+            interface.handle_user_input(user_choice)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print("Exiting application.")
 
 if __name__ == "__main__":
     main()
